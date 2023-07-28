6525d22963192448b48aacb6247af01d374ebf51
gaordonezh <gaordonezh@gmail.com>
2023-07-27 23:36:55 -0500

    request to api

--- 05-sass-http/src/App.vue
+++ 05-sass-http/src/App.vue
@@ -1,5 +1,53 @@
 <template>
   <div class="container">
     <h1>Cotizador de criptomonedas</h1>
+    <grid>
+      <formulario @info-monedas="obtener" />
+      <Data
+        :cripto="info.cripto"
+        :moneda="info.moneda"
+        :img="info.img"
+        :precio="info.precio"
+      />
+    </grid>
   </div>
 </template>
+
+<script>
+import Data from "./components/Data.vue";
+import Formulario from "./components/Formulario.vue";
+import Grid from "./components/Grid.vue";
+
+export default {
+  components: { Formulario, Data, Grid },
+  data: () => ({
+    info: {
+      cripto: "",
+      moneda: "",
+      img: "/media/37746251/btc.png",
+      precio: "0",
+    },
+  }),
+  methods: {
+    async obtener(cripto, moneda) {
+      console.log("here");
+      const res = await fetch(
+        `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${encodeURI(
+          cripto
+        )}&tsyms=${encodeURI(moneda)}`
+      );
+      const { RAW } = await res.json();
+
+      const dataCripto = RAW[cripto];
+      const data = dataCripto[moneda];
+
+      this.info = {
+        cripto: cripto,
+        moneda: moneda,
+        img: data.IMAGEURL,
+        precio: data.PRICE,
+      };
+    },
+  },
+};
+</script>
---
+++ 05-sass-http/src/components/Formulario.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form @submit.prevent="$emit('infoMonedas', cripto, moneda)">
+      <div>
+        <label>
+          Moneda nacional:
+          <select required v-model="moneda">
+            <option value="" disabled selected>Seleccione una moneda...</option>
+            <option>MXN</option>
+            <option>USD</option>
+            <option>EUR</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          Criptomoneda:
+          <select required v-model="cripto">
+            <option value="" disabled selected>
+              Seleccione una criptomoneda...
+            </option>
+            <option>BTC</option>
+            <option>ETH</option>
+            <option>LTC</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <button type="submit">Cotizar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    moneda: "",
+    cripto: "",
+  }),
+};
+</script>
