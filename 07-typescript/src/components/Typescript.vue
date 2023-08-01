0b7d321b6cb2a7a94957c724cc8bc9b3d579276f
gaordonezh <gaordonezh@gmail.com>
2023-07-31 23:02:29 -0500

    component with typescript

---
+++ 07-typescript/src/components/Typescript.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>TYPESCRIPT {{ nombre }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+const Typescript = defineComponent({
+  setup() {
+    const nombre = ref<string>("Aldo");
+    return { nombre };
+  },
+});
+
+export default Typescript;
+</script>
