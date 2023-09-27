421c4981682bbad4680663068f7dbc749b7635b3
Aldo Ordoñez <gaordonezh@gmail.com>
2023-09-26 20:08:31 -0500

    feature: initial pinia config

--- 09-pinia/src/main.js
+++ 09-pinia/src/main.js
@@ -1,16 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import './index.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import './index.css';
 import { Icon } from '@iconify/vue';
 
-const app = createApp(App)
+const app = createApp(App);
 
-const pinia = createPinia()
+const pinia = createPinia();
 
-app.use(router)
-app.use(pinia)
-app.component("Icon", Icon)
+app.use(router);
+app.use(pinia);
+app.component('Icon', Icon);
 
-app.mount('#app')
+app.mount('#app');
